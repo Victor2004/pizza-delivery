54a7ec79ab2cac6e8602d9e931f5a37b551b0a79
Victor <victorp2908@gmail.com>
2024-05-30 23:06:07 +0300

    меню редактирования продукта

--- client/src/components/EditMenu.tsx
+++ client/src/components/EditMenu.tsx
@@ -16,25 +16,33 @@ export function ModalEdit() {
 
     return (
         <>
-            <button onClick={toggleModal} className="btn-modal">
-                Open
+            <button className="edit-btn" onClick={toggleModal}>
+                <i className="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                    </svg>
+                </i>
             </button>
 
             {modal && (
                 <div className="modal">
                     <div onClick={toggleModal} className="overlay"></div>
                     <div className="modal-content">
-                        <h2>Hello Modal</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-                            perferendis suscipit officia recusandae, eveniet quaerat assumenda
-                            id fugit, dignissimos maxime non natus placeat illo iusto!
-                            Sapiente dolorum id maiores dolores? Illum pariatur possimus
-                            quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-                            placeat tempora vitae enim incidunt porro fuga ea.
-                        </p>
+                        <h2>Редактироватие товара</h2>
+                        <form>
+                            <label htmlFor="ProductID">ProductID </label>
+                            <input type="text" id="ProductID" placeholder="id" />
+                        </form>
+                        <form>
+                            <label htmlFor="ProductName">ProductName </label>
+                            <input type="text" id="ProductName" placeholder="Название продукта" />
+                        </form>
+                        <form>
+                            <label htmlFor="Price">Price </label>
+                            <input type="text" id="Price" placeholder="Цена" />
+                        </form>
                         <button className="close-modal" onClick={toggleModal}>
-                            CLOSE
+                            X
                         </button>
                     </div>
                 </div>
--- client/src/components/MenuContainers.tsx
+++ client/src/components/MenuContainers.tsx
@@ -12,13 +12,7 @@ function MenuContainers(props: { id: number, name: string, price: number }) {
             <div className="pizza-info">
                 <h2>{name}</h2>
                 <p>Цена: {price} руб.</p>
-                <button className="edit-btn" onClick={ModalEdit}>
-                    <i className="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
-                        </svg>
-                    </i>
-                </button>
+                <ModalEdit />
                 <button className="delete-btn">
                     <i className="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
@@ -36,7 +30,6 @@ export default function Menu() {
     // alert(products[0].ProductID);
     return (
         <>
-            <ModalEdit />
             <div className="menu-grid">
                 {products.map(product => (
                     <MenuContainers id={1} name={product.ProductName} price={product.Price} />
